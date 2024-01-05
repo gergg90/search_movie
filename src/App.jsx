96146ce8d9b01108503c3c7879b0a8859ff655fb
GerGG <germain.gg90@gmail.com>
2024-01-05 08:31:09 -0400

    creando plantilla html en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import './app.css'
 
 // ! http://www.omdbapi.com/?apikey=67ae54d&title=rocky
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="page">
+
+
+      <header>
+        <h1>Buscador de peliculas.</h1>
+        <form className="form">
+          <input type="buttom" placeholder="Write your movie." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+
+
+      <main>
+        <p>Aqui iran las Movies</p>
+      </main>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
